1ea754c841ea5fb83a32e2f5fd411c9b8b83461d
AlexHofman <alexxhhofman@gmail.com>
2024-10-10 09:24:13 +0100

    password hashing

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,50 @@
 // Create a new router
-const express = require("express")
-const router = express.Router()
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const plainPassword = req.body.password;
 
-router.get('/register', function (req, res, next) {
-    res.render('register.ejs')                                                               
-})    
+const express = require("express");
+const router = express.Router();
 
-router.post('/registered', function (req, res, next) {
-    // saving data in database
-    res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email)                                                                           
-})
+router.get("/register", function (req, res, next) {
+  res.render("register.ejs");
+});
+
+router.post("/registered", function (req, res, next) {
+  // saving data in database
+  bcrypt.hash(plainPassword, saltRounds, function (err, hashedPassword) {
+    // Store hash in your password DB.
+    let sqlquery =
+      "INSERT INTO users (username, first_name, last_name, password) VALUES (?,?,?)";
+    let newrecord = [
+      req.body.first,
+      req.body.last,
+      req.body.email,
+      req.body.username,
+      hashedPassword,
+    ];
+    db.query(sqlquery, newrecord, (err, result) => {
+      if (err) {
+        next(err);
+      } else {
+        result =
+          "Hello " +
+          req.body.first +
+          " " +
+          req.body.last +
+          " you are now registered!  We will send an email to you at " +
+          req.body.email;
+        result +=
+          "Your password is: " +
+          req.body.password +
+          " and your hashed password is: " +
+          hashedPassword;
+        res.send(result);
+      }
+    });
+    console.log(hash);
+  });
+});
 
 // Export the router object so index.js can access it
-module.exports = router
+module.exports = router;
