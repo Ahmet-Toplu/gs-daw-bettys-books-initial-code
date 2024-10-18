23d88ce272b0bdd349e17458cbb3c38b33b5d598
AlexHofman <alexxhhofman@gmail.com>
2024-10-18 14:03:43 +0100

    update

--- routes/users.js
+++ routes/users.js
@@ -45,5 +45,45 @@ router.post("/registered", function (req, res, next) {
   });
 });
 
+router.get("/listusers", function (req, res, next) {
+  // everything from users table except for password
+  let sqlquery = "SELECT username, first_name, last_name, email FROM users";
+  // execute sql query
+  db.query(sqlquery, (err, result) => {
+    if (err) {
+      next(err);
+    }
+    res.render("listusers.ejs", { availableUsers: result });
+  });
+});
+
+router.get("/login", function (req, res, next) {
+  res.render("login.ejs");
+});
+
+router.post("/loggedin", function (req, res, next) {
+  let sqlquery = "SELECT username, password FROM users";
+  let record = [req.body.username, req.body.password];
+  db.query(sqlquery, record, (err, result) => {
+    if (err) {
+      next(err);
+    } else {
+      bcrypt.compare(
+        req.body.password,
+        result[0].password,
+        function (err, result) {
+          if (err) {
+            next(err);
+          } else if (result == true) {
+            res.send("You are now logged in as " + req.body.username);
+          } else {
+            res.send("Invalid password");
+          }
+        }
+      );
+    }
+  });
+});
+
 // Export the router object so index.js can access it
 module.exports = router;
